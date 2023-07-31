0d28a92683a72697988d8593f0673767dc274bd2
Kughan Nadarajah <kughankay@gmail.com>
2023-07-31 15:59:43 -0400

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
-
         public string UserName { get; set; } = string.Empty;
+        public byte[]? PasswordHash { get; set; }
+        public byte[]? PasswordSalt { get;set; } 
     }
 }
